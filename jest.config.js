df9e813a6f7d61432d97b582a0ac61a1c72cead4
shravanisdakve <shravanisdakve@gmail.com>
2026-05-03 21:42:18 +0530

    chore: update jest.config.js for code quality and accessibility hardening

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ export default {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/fileMock.js',
   },
   transform: {
     // Use babel-jest for .js, .jsx, .ts, .tsx files
